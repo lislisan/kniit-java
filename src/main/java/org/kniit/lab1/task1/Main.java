037d9aa6588ec7b863e75d384ad5c490954502cc
lislis <andreevaelisavet@yandex.ru>
2025-09-07 12:08:26 -0700

    Update Main.java

--- src/main/java/org/kniit/lab1/task1/Main.java
+++ src/main/java/org/kniit/lab1/task1/Main.java
@@ -2,7 +2,7 @@ package org.kniit.lab1.task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("___Вариант 1 (не самый удачный)___");
+        System.out.println("___Вариант 1___");
         SolutionFirst.PrintNumbers();
 
         System.out.println("\n___Вариант 2___");
